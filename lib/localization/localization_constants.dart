6c50adf0ece34c2c1fe7408c1b440c82f7a2d4e8
imsajib02 <shafiul.imsajib@gmail.com>
2024-05-27 11:33:48 +0600

    App localization

---
+++ lib/localization/localization_constants.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+const String ENGLISH = 'en';
+const String BANGLA = 'bn';
+
+Locale getLocale(String languageCode) {
+
+  Locale _locale;
+
+  switch(languageCode) {
+
+    case ENGLISH:
+      _locale = Locale(languageCode, 'US');
+      break;
+
+    case BANGLA:
+      _locale = Locale(languageCode, 'BD');
+      break;
+
+    default:
+      _locale = Locale(ENGLISH, 'US');
+  }
+
+  return _locale;
+}
+
+List<Locale> get supportedLocales => [
+  Locale(ENGLISH, 'US'),
+  Locale(BANGLA, 'BD'),
+];
