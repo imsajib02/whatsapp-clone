f566f612e4efbde87ebe594573f1443e8abb6acf
imsajib02 <shafiul.imsajib@gmail.com>
2024-05-27 17:43:11 +0600

    App routes updated

--- lib/route/routes.dart
+++ lib/route/routes.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../barrel/pages.dart';
+import '../main.dart';
 
 final String INITIAL = '/';
+final String LOGIN = '/login';
+final String HOME = '/home';
 
 final router = GoRouter(
   initialLocation: INITIAL,
@@ -12,16 +15,28 @@ final router = GoRouter(
     GoRoute(
       name: INITIAL,
       path: INITIAL,
+      builder: (context, state) => SplashScreen(),
+    ),
+
+    GoRoute(
+      name: LOGIN,
+      path: LOGIN,
       builder: (context, state) => Login(),
+    ),
+
+    GoRoute(
+      name: HOME,
+      path: HOME,
+      builder: (context, state) => Home(),
     ),
   ],
   redirect: (BuildContext context, GoRouterState state) {
 
     final bool initialLocation = state.subloc == INITIAL;
 
-    /*if(initialLocation && currentUser != null && currentUser!.isLoggedIn()) {
-      return DASHBOARD;
-    }*/
+    if(initialLocation && authUser != null) {
+      return HOME;
+    }
 
     return null;
   },
