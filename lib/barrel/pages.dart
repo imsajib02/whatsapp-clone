426ede4b5af6bd0b11eddfa983372f49d7dc4b4c
imsajib02 <shafiul.imsajib@gmail.com>
2024-05-28 12:18:57 +0600

    Pages barrel files updated

--- lib/barrel/pages.dart
+++ lib/barrel/pages.dart
@@ -1,3 +1,7 @@
 export '../pages/login/login.dart';
 export '../pages/splash/splash_screen.dart';
 export '../pages/home/home.dart';
+export '../pages/chats/chats.dart';
+export '../pages/updates/updates.dart';
+export '../pages/communities/communities.dart';
+export '../pages/calls/calls.dart';
